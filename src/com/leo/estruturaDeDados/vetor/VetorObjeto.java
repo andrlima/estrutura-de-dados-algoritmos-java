56b4e36665140a412afe473073bbfac0f5661e9b
leo araujo <tc.leo.araujo@gmail.com>
2020-09-08 09:56:02 -0300

    Aula 10

--- src/com/leo/estruturaDeDados/vetor/VetorObjeto.java
+++ src/com/leo/estruturaDeDados/vetor/VetorObjeto.java
@@ -5,11 +5,30 @@ public class VetorObjeto {
 	private Object[] elementos;
 	private int tamanho;
 	
+	//MÉTODO CONSTRUTOR
 	public VetorObjeto(int capacidade) {
 		
 		this.elementos = new Object[capacidade];
 		this.tamanho = 0;
 	}
 	
+	
+	//MÉTODO ADICIONAR. Adiciona valores no vetor.
+	public boolean adocionar(Object elemento) {
+				
+		if(this.tamanho < this.elementos.length) {
+			
+			this.elementos[this.tamanho]= elementos;
+			this.tamanho++; //qtd do vetor
+			
+			//Se adicionou
+			return true;
+		}
+		
+		//Se não adicionou
+		return false;
+		
+		
+	}
 
 }
